2c98e550aaf87edad9deebbf1b89b49616a1c1ab
Gunfil <jaxor@mail.ru>
2021-07-15 20:22:33 +0300

    Initial commit

---
+++ src/main/java/ru/rmt/crud/config/MySpringMVCDispatcherServletInitializer.java
@@ -0,0 +1,23 @@
+package ru.rmt.crud.config;
+
+import org.springframework.lang.Nullable;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMVCDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Nullable
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Nullable
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
